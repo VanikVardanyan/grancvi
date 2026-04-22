6f1f2fc653ed07be5faee3df34686a85a04da967
v.vardanyan <v.vardanyan@aitarget.com>
2026-04-22 14:17:56 +0400

    Revert "chore(debug): temporary /sentrytest handler for AC7 smoke"
    This reverts commit b05725e360ade4294df12120f913c62964dbb32f.

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -2,14 +2,12 @@ from __future__ import annotations
 
 from aiogram import Router
 
-from src.handlers._sentry_debug import router as sentry_debug_router
 from src.handlers.client import router as client_router
 from src.handlers.master import router as master_router
 
 
 def build_root_router() -> Router:
     root = Router(name="root")
-    root.include_router(sentry_debug_router)
     root.include_router(master_router)
     root.include_router(client_router)
     return root
